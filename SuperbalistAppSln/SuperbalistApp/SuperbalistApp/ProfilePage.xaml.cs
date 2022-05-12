a6ffa425f8d9ebd5d299008e2e5818cd2680d8bb
LaurenArendse <102674344+LaurenArendse@users.noreply.github.com>
2022-05-12 15:33:00 +0200

    Completed superbalist pages

---
+++ SuperbalistAppSln/SuperbalistApp/SuperbalistApp/ProfilePage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SuperbalistApp
+{
+
+    public class Option
+    {
+        public string OptionItem { get; set; }
+
+        public string Icon { get; set; }
+
+        public Option(string optionItem, string icon)
+        {
+            OptionItem = optionItem;
+            Icon = icon;
+        }
+    }
+
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+
+        public ObservableCollection<Option> Options { get; set; }
+        public ProfilePage()
+        {
+            InitializeComponent();
+
+            Options = new ObservableCollection<Option>
+            {
+                new Option("Track my order", "trackIcon.png"),
+                new Option("Log a Return or Exchange", "exchangeIcon.png"),
+                new Option("Account", "profileIcon.png"),
+                new Option("Help", "helpIcon.png"),
+                new Option("Takealot.group", "takealotIcon.png"),
+                new Option("    Sign Out", ""),
+                new Option("    Rate the app", ""),
+            };
+
+            BindingContext = this;
+
+        }
+    }
+}
---
+++ SuperbalistAppSln/SuperbalistApp/SuperbalistApp/SearchPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SuperbalistApp
+{
+    public class Category
+    {
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        //public string Image { get; set; }
+
+        public Category(string name, string color)
+        {
+            Name = name;
+            Color = color;
+            
+        }
+
+        
+    }
+
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchPage : ContentPage
+    {
+        public ObservableCollection<Category> Categories { get; set; }
+
+        public SearchPage()
+        {
+            InitializeComponent();
+
+            Categories = new ObservableCollection<Category>
+            {
+                new Category("OFFERS", "LightGreen"),
+                new Category("brands", "lightgray"),
+                new Category("new in", "lightgray"),
+                new Category("accessories", "lightgray"),
+                new Category("beauty", "lightgray"),
+                new Category("bottoms", "lightgray"),
+                new Category("dresses", "lightgray"),
+                new Category("jackets & coats", "lightgray"),
+                new Category("lingerie & sleepwear", "lightgray"),
+                new Category("maternity", "lightgray"),
+                new Category("petite", "lightgray"),
+                new Category("plus size", "lightgray"),
+                new Category("shoes", "lightgray"),
+                new Category("sport", "lightgray"),
+                new Category("swimwear", "lightgray"),
+                new Category("tops", "lightgray"),
+                new Category("sale", "lightgray"),
+            };
+
+            BindingContext = this;
+        }
+    }
+}
